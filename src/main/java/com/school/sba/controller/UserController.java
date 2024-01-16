d1930d9dca471f8a5fae715b67532b4a12226c74
vishwas5968 <vishwasr968@gmail.com>
2024-01-16 14:03:21 +0530

    made changes to service class

--- src/main/java/com/school/sba/controller/UserController.java
+++ src/main/java/com/school/sba/controller/UserController.java
@@ -1,7 +1,6 @@
 package com.school.sba.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,8 +9,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.school.sba.dto.UserRequest;
-import com.school.sba.enums.UserRole;
-import com.school.sba.exception.ConstraintViolationException;
 import com.school.sba.service.UserService;
 
 @RestController
@@ -20,19 +17,9 @@ public class UserController {
 	@Autowired
 	UserService userService;
 
-	int count;
-
 	@PostMapping(path = "/users/register")
 	public Object registerUser(@RequestBody @Validated UserRequest userRequest) {
-		if (userRequest.getUserRole() == UserRole.ADMIN) {
-			count += 1;
-		}
-		if (count == 1 || userRequest.getUserRole()!=UserRole.ADMIN) {
-			return userService.register(userRequest);
-		} else {
-			throw new ConstraintViolationException("There is already an admin", HttpStatus.IM_USED,
-					"More than 1 admin is not allowed");
-		}
+		return userService.register(userRequest);
 	}
 
 	@DeleteMapping(path = "/users/{userId}")
--- src/main/java/com/school/sba/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/school/sba/serviceImpl/UserServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import com.school.sba.dto.UserRequest;
 import com.school.sba.entity.User;
+import com.school.sba.enums.UserRole;
 import com.school.sba.exception.ConstraintViolationException;
 import com.school.sba.exception.UserNotFoundException;
 import com.school.sba.repository.UserRepo;
@@ -13,19 +14,30 @@ import com.school.sba.service.UserService;
 
 @Service
 public class UserServiceImpl implements UserService {
-	
+
 	@Autowired
 	UserRepo userRepo;
 
+	int count;
+
 	@Override
 	public Object register(UserRequest userRequest) {
-		User user=mapToUser(userRequest);
-		try {
-			user=userRepo.save(user);			
-		} catch (Exception e) {
-			throw new ConstraintViolationException("Duplicate Entry made", HttpStatus.IM_USED,
-					"No duplicate entries are allowed");
+		User user = mapToUser(userRequest);
+		if (userRequest.getUserRole() == UserRole.ADMIN) {
+			count += 1;
 		}
+		if (count == 1 || userRequest.getUserRole() != UserRole.ADMIN) {
+			try {
+				user = userRepo.save(user);
+			} catch (Exception e) {
+				throw new ConstraintViolationException("Duplicate Entry made", HttpStatus.IM_USED,
+						"No duplicate entries are allowed");
+			}
+		} else {
+			throw new ConstraintViolationException("There is already an admin", HttpStatus.IM_USED,
+					"More than 1 admin is not allowed");
+		}
+
 		return "User Saved Successfully";
 	}
 
@@ -38,11 +50,12 @@ public class UserServiceImpl implements UserService {
 
 	@Override
 	public Object deleteUser(int userId) {
-		User user=new User();
+		User user = new User();
 		try {
-			user=userRepo.findById(userId).get();			
+			user = userRepo.findById(userId).get();
 		} catch (Exception e) {
-			throw new UserNotFoundException("User with given ID is not registered in the database",HttpStatus.NOT_FOUND, "No such user in database");
+			throw new UserNotFoundException("User with given ID is not registered in the database",
+					HttpStatus.NOT_FOUND, "No such user in database");
 		}
 		userRepo.delete(user);
 		return "User Deleted Successfully";
